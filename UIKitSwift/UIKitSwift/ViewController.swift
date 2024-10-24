591646f70ed1146ba3b16ed30321a0987fd4c9d6
ghsumiyasu <myemail@gmail.com>
2024-10-24 20:36:51 +0900

    Teste do safeAreaLayoutGuide

--- UIKitSwift/UIKitSwift/ViewController.swift
+++ UIKitSwift/UIKitSwift/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var backgroundView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setView()
     }
-
+    
+    private func setView() {
+        view.backgroundColor = .red
+        setHierarchy()
+        setConstrains()
+    }
+    private func setHierarchy() {
+        view.addSubview(backgroundView)
+    }
+    private func setConstrains() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])    }
 
 }
 
